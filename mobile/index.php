8267287456b9e0baf436742115161852eb517dd1
Wojtuch566 <dromawojciech@gmail.com>
2025-01-11 17:38:50 +0100

    Podział na wersję desktopową i mobilną

---
+++ mobile/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Plan ZUT 2024</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
